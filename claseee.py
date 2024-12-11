ccafabe756d1afe5aaac3665321a16757852b5e9
Carlos Lechuga <carloslechugaaa@gmail.com>
2024-12-11 16:55:54 -0600

    clase.py

--- claseee.py
+++ claseee.py
@@ -1,31 +1,20 @@
-class Paquete:
-    def __init__(self):
-        self.__peso = 0
-        self.__tarifaBase = 200
-        self.__tarifaAdicionalPorKg = 40
-        self.__tarifaEspecial = 300
-        self.__costoTotal = 0 
-
-    def calcularCostoTotal(self, peso):
+class paquete():
+    __peso = 0
+    __tarifaBase = 200
+    __tarifaAdicionalPorKg = 40
+    __tarifaEspecial = 300
+    __costoTotal = 0  
+    
+    def calcularCosto(self, peso):
         if peso <= 0:
             raise ValueError("Ingresa números reales.")
-
-        self.__peso = peso 
-        
- 
-        if peso <= 5:
+        elif peso <= 5:
             self.__costoTotal = self.__tarifaBase
         elif peso <= 20:
             self.__costoTotal = self.__tarifaBase + (peso - 5) * self.__tarifaAdicionalPorKg
         else:
-            self.__costoTotal = (
-                self.__tarifaBase + 
-                (20 - 5) * self.__tarifaAdicionalPorKg + 
-                self.__tarifaEspecial
-            )
-
+            self.__costoTotal = self.__tarifaBase + (20 - 5) * self.__tarifaAdicionalPorKg + self.__tarifaEspecial
         return self.__costoTotal
-
-    def obtenerCostoTotal(self):
-        
-        return self.__costoTotal
+            
+    def TarifaAdicional(self):
+         return self.__costoTotal
